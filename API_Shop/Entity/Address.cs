68c073311d0f3ca8f65465d4a7fe99d71bac0a21
8b477 <jonathan_buchet@outlook.fr>
2024-06-23 18:02:19 +0200

    :wrench: Add Data Annotation

--- API_Shop/Entity/Address.cs
+++ API_Shop/Entity/Address.cs
@@ -1,12 +1,35 @@
-﻿namespace API_Shop.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API_Shop.Models
 {
     public class Address
     {
-        public int    Id           { get; set; }
-        public int    IdUser       { get; set; }
-        public int    PostalCode   { get; set; }
-        public int    StreetNumber { get; set; }
-        public string Country      { get; set; } = string.Empty;
-        public string City         { get; set; } = string.Empty;
+        [Key]
+        public int Id { get; set; }
+
+
+        [ForeignKey(nameof(IdUser))]
+        public int IdUser { get; set; }
+
+
+        [DataType(DataType.PostalCode, ErrorMessage = "Le code postal n'est pas valide !")]
+        [Required(ErrorMessage = "Code postal requis")]
+        public int PostalCode { get; set; }
+
+
+        [Required(ErrorMessage = "Numéro de rue requis")]
+        [Range(1, 9401, ErrorMessage = "Le numéro de rue doit être compris entre 1 et 9401")]
+        public int StreetNumber { get; set; }
+
+
+        [MaxLength(35)]
+        [Required(ErrorMessage = "Pays requis")]
+        public string Country { get; set; } = string.Empty;
+
+
+        [MaxLength(85)]
+        [Required(ErrorMessage = "La ville est requis")]
+        public string City { get; set; } = string.Empty;
     }
 }
